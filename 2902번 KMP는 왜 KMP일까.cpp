0f122281efeae80f4992501cf246191589c938a4
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2020-08-27 21:47:33 +0900

    BOJ PS

--- 2902번 KMP는 왜 KMP일까.cpp
+++ 2902번 KMP는 왜 KMP일까.cpp
@@ -1,15 +1,15 @@
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//
-//int main(void) {
-//	string ret, str;
-//	cin >> str;
-//	ret += str[0];
-//
-//	for (int i = 0; i < str.size(); i++)
-//		if (str[i] == '-')
-//			ret += str[i + 1];
-//
-//	cout << ret;
-//}
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void) {
+	string ret, str;
+	cin >> str;
+	ret += str[0];
+
+	for (int i = 0; i < str.size(); i++)
+		if (str[i] == '-')
+			ret += str[i + 1];
+
+	cout << ret;
+}
