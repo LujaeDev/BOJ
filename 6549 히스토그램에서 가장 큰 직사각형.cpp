e735d47a4c359b6af600a8ab0142bf88dea7fdfa
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2020-08-27 21:48:50 +0900

    BOJ PS

--- 6549 히스토그램에서 가장 큰 직사각형.cpp
+++ 6549 히스토그램에서 가장 큰 직사각형.cpp
@@ -1,47 +1,47 @@
-//#include<iostream>
-//#include<algorithm>
-//#include<stack>
-//
-//using namespace std;
-//int h[100001];
-//
-//long long solve(int n) {
-//	long long ret = 0;
-//
-//	stack<int> remain;
-//	h[n] = 0;
-//
-//	for (int i = 0; i < n + 1; i++) {
-//		while (!remain.empty() && h[remain.top()] >= h[i]) {
-//			int idx = remain.top();
-//			remain.pop();
-//
-//			int width = 0;
-//
-//			if (remain.empty())
-//				width = i;
-//			else
-//				width = i - remain.top() - 1;
-//			
-//			ret = max(ret, (long long)h[idx] * width);
-//		}
-//
-//		remain.push(i);
-//	}
-//	
-//	return ret;
-//}
-//
-//int main(void) {
-//	int n = 1;
-//
-//	scanf("%d", &n);
-//
-//	while (n != 0) {
-//		for (int i = 0; i < n; i++)
-//			scanf("%d", &h[i]);
-//		
-//		printf("%lld\n", solve(n));
-//		scanf("%d", &n);
-//	}
-//}
+#include<iostream>
+#include<algorithm>
+#include<stack>
+
+using namespace std;
+int h[100001];
+
+long long solve(int n) {
+	long long ret = 0;
+
+	stack<int> remain;
+	h[n] = 0;
+
+	for (int i = 0; i < n + 1; i++) {
+		while (!remain.empty() && h[remain.top()] >= h[i]) {
+			int idx = remain.top();
+			remain.pop();
+
+			int width = 0;
+
+			if (remain.empty())
+				width = i;
+			else
+				width = i - remain.top() - 1;
+			
+			ret = max(ret, (long long)h[idx] * width);
+		}
+
+		remain.push(i);
+	}
+	
+	return ret;
+}
+
+int main(void) {
+	int n = 1;
+
+	scanf("%d", &n);
+
+	while (n != 0) {
+		for (int i = 0; i < n; i++)
+			scanf("%d", &h[i]);
+		
+		printf("%lld\n", solve(n));
+		scanf("%d", &n);
+	}
+}
