06046f0a4172c2474d456983c700d7adc32cf830
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2020-08-27 21:44:27 +0900

    BOJ PS

--- 12015 가장 긴 증가하는 부분 수열2.cpp
+++ 12015 가장 긴 증가하는 부분 수열2.cpp
@@ -1,54 +1,54 @@
-//#include<iostream>
-//#include<vector>
-//#include<algorithm>
-//
-//using namespace std;
-//
-//int arr[1000000], p[1000000];
-//vector<int> v, ret;
-//
-//int main(void) {
-//	int num;
-//	vector<int>::iterator low;
-//
-//	cin >> num;
-//
-//	for (int i = 0; i < num; ++i)
-//		scanf("%d", &arr[i]);
-//
-//	for (int here = 0; here < num; here++) {
-//		if (v.empty()) {
-//			v.push_back(arr[here]);
-//			p[here] = 0;
-//		}
-//		else {
-//			if (v.back() < arr[here]) {
-//				v.push_back(arr[here]);
-//				p[here] = v.size() - 1;
-//			}
-//			else {
-//				low = lower_bound(v.begin(), v.end(), arr[here]);
-//				*low = arr[here];
-//				p[here] = low - v.begin();
-//			}
-//		}
-//	}
-//
-//	cout << v.size() << endl;
-//	int cnt = v.size() - 1;
-//
-//	for (int i = num - 1; i >= 0; i--) {
-//		if (p[i] == cnt) {
-//			ret.push_back(arr[i]);
-//			cnt--;
-//		}
-//		if (cnt == -1)
-//			break;
-//	}
-//	
-//	for (auto i : ret) {
-//		printf("%d ", ret.back());
-//		ret.pop_back();
-//	}
-//	return 0;
-//}
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int arr[1000000], p[1000000];
+vector<int> v, ret;
+
+int main(void) {
+	int num;
+	vector<int>::iterator low;
+
+	cin >> num;
+
+	for (int i = 0; i < num; ++i)
+		scanf("%d", &arr[i]);
+
+	for (int here = 0; here < num; here++) {
+		if (v.empty()) {
+			v.push_back(arr[here]);
+			p[here] = 0;
+		}
+		else {
+			if (v.back() < arr[here]) {
+				v.push_back(arr[here]);
+				p[here] = v.size() - 1;
+			}
+			else {
+				low = lower_bound(v.begin(), v.end(), arr[here]);
+				*low = arr[here];
+				p[here] = low - v.begin();
+			}
+		}
+	}
+
+	cout << v.size() << endl;
+	int cnt = v.size() - 1;
+
+	for (int i = num - 1; i >= 0; i--) {
+		if (p[i] == cnt) {
+			ret.push_back(arr[i]);
+			cnt--;
+		}
+		if (cnt == -1)
+			break;
+	}
+	
+	for (auto i : ret) {
+		printf("%d ", ret.back());
+		ret.pop_back();
+	}
+	return 0;
+}
