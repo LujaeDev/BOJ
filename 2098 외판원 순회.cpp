7733c3a35d2efcfd485c62221b1547e1aac99be0
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2020-08-27 21:07:38 +0900

    BOJ PS

--- 2098 외판원 순회.cpp
+++ 2098 외판원 순회.cpp
@@ -1,45 +1,45 @@
-//#include<iostream>
-//#include<vector>
-//#include<algorithm>
-//#include<cstring>
-//using namespace std;
-//
-//#define INF 987654321
-//int W[16][16];
-//int cache[16][1 << 16];
-//
-//int TSP(int pos, int visited, int num) {
-//	int& ret = cache[pos][visited];
-//
-//	if (visited == (1 << num) - 1) {
-//		if (W[pos][0] == 0)
-//			return INF;
-//		return W[pos][0];
-//	}
-//	else if (cache[pos][visited] != -1)
-//		return cache[pos][visited];
-//
-//	ret = INF;
-//
-//	for (int next = 1; next < num; next++) {
-//		if (!(visited & (1 << next)) && W[pos][next] != 0)
-//			ret = min(ret, TSP(next, visited | (1 << next), num) + W[pos][next]);
-//	}
-//
-//	return cache[pos][visited] = ret;
-//}
-//
-//int main(void) {
-//	int num;
-//
-//	cin >> num;
-//
-//	for (int i = 0; i < num; i++)
-//		for (int j = 0; j < num; j++)
-//			scanf("%d", &W[i][j]);
-//
-//	memset(cache, -1, sizeof(cache));
-//
-//	
-//	cout << TSP(0, 1, num);
-//}
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+#define INF 987654321
+int W[16][16];
+int cache[16][1 << 16];
+
+int TSP(int pos, int visited, int num) {
+	int& ret = cache[pos][visited];
+
+	if (visited == (1 << num) - 1) {
+		if (W[pos][0] == 0)
+			return INF;
+		return W[pos][0];
+	}
+	else if (cache[pos][visited] != -1)
+		return cache[pos][visited];
+
+	ret = INF;
+
+	for (int next = 1; next < num; next++) {
+		if (!(visited & (1 << next)) && W[pos][next] != 0)
+			ret = min(ret, TSP(next, visited | (1 << next), num) + W[pos][next]);
+	}
+
+	return cache[pos][visited] = ret;
+}
+
+int main(void) {
+	int num;
+
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+		for (int j = 0; j < num; j++)
+			scanf("%d", &W[i][j]);
+
+	memset(cache, -1, sizeof(cache));
+
+	
+	cout << TSP(0, 1, num);
+}
