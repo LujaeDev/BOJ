a5709044b7a4a6f4bda70b46cb689a482f1dc868
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2020-08-27 21:47:14 +0900

    BOJ PS

--- 2875번 대회 or 인턴.cpp
+++ 2875번 대회 or 인턴.cpp
@@ -1,20 +1,20 @@
-//#include<iostream>
-//#include<algorithm>
-//
-//using namespace std;
-//
-//int main(void) {
-//	int N, M, K;
-//	
-//	cin >> N >> M >> K;
-//
-//	while (K--) {
-//		if (N >= 2 * M)
-//			N--;
-//		else
-//			M--;
-//	}
-//	int ret = (N / 2) > M ? M : N / 2;
-//	cout << ret;
-//	return 0;
-//}
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main(void) {
+	int N, M, K;
+	
+	cin >> N >> M >> K;
+
+	while (K--) {
+		if (N >= 2 * M)
+			N--;
+		else
+			M--;
+	}
+	int ret = (N / 2) > M ? M : N / 2;
+	cout << ret;
+	return 0;
+}
