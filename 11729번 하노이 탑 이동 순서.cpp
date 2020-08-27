eea18eae000e8cac10db1658fd426385222d8841
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2020-08-27 21:43:38 +0900

    BOJ PS

--- 11729번 하노이 탑 이동 순서.cpp
+++ 11729번 하노이 탑 이동 순서.cpp
@@ -1,27 +1,27 @@
-//#include<iostream>
-//#include<vector>
-//#include<algorithm>
-//#include<math.h>
-//using namespace std;
-//
-//void Hanoi(int k, int from, int aux, int to) {
-//	if (k == 1) {
-//		cout << from << " " << to << "\n";
-//		return;
-//	}
-//	
-//	Hanoi(k - 1, from, to, aux);
-//	cout << from << " " << to << "\n";
-//	Hanoi(k - 1, aux, from, to);
-//
-//}
-//int main(void) {
-//	int k;
-//
-//	cin >> k;
-//
-//	cout << (int)pow(2, k) - 1 << endl;
-//	Hanoi(k, 1, 2, 3);
-//
-//	return 0;
-//}
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+void Hanoi(int k, int from, int aux, int to) {
+	if (k == 1) {
+		cout << from << " " << to << "\n";
+		return;
+	}
+	
+	Hanoi(k - 1, from, to, aux);
+	cout << from << " " << to << "\n";
+	Hanoi(k - 1, aux, from, to);
+
+}
+int main(void) {
+	int k;
+
+	cin >> k;
+
+	cout << (int)pow(2, k) - 1 << endl;
+	Hanoi(k, 1, 2, 3);
+
+	return 0;
+}
