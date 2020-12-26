7a3b3bcf593c95d7f2f26982c94926dc63a352eb
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2020-12-26 20:58:38 +0900

    BOJ PS

---
+++ JAVA/BOJ1149.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    static int [][]cost;
+    static int [][]cache;
+
+    static public int sol(int idx, int beforeColor){
+        if(idx >= cost.length)
+            return 0;
+
+        int ret = cache[idx][beforeColor];
+
+        if(ret != 0)
+            return ret;
+
+        ret = 987654321;
+
+        for(int i = 0; i < 3; ++i){
+            if(i != beforeColor || idx == 0)
+                ret = Math.min(ret, cost[idx][i] + sol(idx + 1, i));
+        }
+
+        return cache[idx][beforeColor] = ret;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        cost = new int[n][3];
+        cache = new int[n][3];
+
+        for(int i = 0; i < n; ++i){
+            String arrayS[] = br.readLine().split(" ");
+
+            for(int j = 0; j < 3; ++j)
+                cost[i][j] = Integer.parseInt(arrayS[j]);
+        }
+
+        System.out.print(sol(0, 0));
+
+        br.close();
+    }
+}
