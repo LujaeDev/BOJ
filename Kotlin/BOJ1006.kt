5da9025675315b6b7cd08577275e98e089910bea
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2021-01-16 23:43:52 +0900

    BOJ PS

--- Kotlin/BOJ1006.kt
+++ Kotlin/BOJ1006.kt
@@ -1,36 +1,59 @@
 import java.io.*
 
-fun sol(enemy: Array<List<Int>>, W: Int): Int{
+fun sol(enemy: Array<ArrayList<Int>>, W: Int): Int{
     val N = enemy[0].size
-
+    val INF = W + 1
     val dp = Array<Array<Int>>(N + 1){ Array<Int>(4){0} }
-    val dp2 = Array<Array<Int>>(N + 1){ Array<Int>(4){0} }
+    var maxPair = 0
+    val canCircle = Array<Boolean>(4){false}
+    val rightE0 = enemy[0][N - 1]
+    val rightE1 = enemy[1][N - 1]
 
-    dp[0][0] = if(W >= enemy[0][N - 1] + enemy[1][N - 1]) 1 else 0
-    dp[0][1] = if(W >= enemy[1][0] + enemy[1][N - 1]) 1 else 0
-    dp[0][2] = if(W >= enemy[0][0] + enemy[0][N - 1]) 1 else 0
-    dp[0][3] = maxOf(if(W >= enemy[0][0] + enemy[1][0]) 1 else 0, if(dp[0][1] + dp[0][2] == 2) 2 else 0)
+    if(N == 1){
+        maxPair = if(W >= enemy[0][0] + enemy[1][0]) 1 else 0
+    }else {
+        canCircle[0] = true
+        canCircle[1] = W >= enemy[0][0] + enemy[0][N - 1]
+        canCircle[2] = W >= enemy[1][0] + enemy[1][N - 1]
+        canCircle[3] = canCircle[1] && canCircle[2]
 
-    for(i in 1 until N - 1){
-        dp[i][0] = dp[i - 1].maxOrNull()!!
-        dp[i][1] = if(W >= enemy[1][i - 1] + enemy[1][i]) maxOf(dp[i - 1][0], dp[i - 1][2]) + 1 else 0
-        dp[i][2] = if(W >= enemy[0][i - 1] + enemy[0][i]) maxOf(dp[i - 1][0], dp[i - 1][1]) + 1 else 0
-        dp[i][3] = if(W >= enemy[0][i] + enemy[1][i]) dp[i][0] + 1 else 0
-        dp[i][3] = if(W >= enemy[1][i - 1] + enemy[1][i] && W >= enemy[0][i - 1] + enemy[0][i]) maxOf(dp[i][3], dp[i - 1][0] + 2) else dp[i][3]
+        for (k in canCircle.indices) {
+            if (!canCircle[k])
+                continue
+
+            enemy[0][N - 1] = rightE0
+            enemy[1][N - 1] = rightE1
+
+            for (i in 1..3)
+                dp[0][i] = 0
+
+            if (k == 1) {
+                dp[0][2] = 1
+                enemy[0][N - 1] = INF
+            } else if (k == 2) {
+                dp[0][1] = 1
+                enemy[1][N - 1] = INF
+            } else if (k == 3) {
+                dp[0][3] = 2
+                enemy[0][N - 1] = INF
+                enemy[1][N - 1] = INF
+            }
+
+            dp[0][3] = maxOf(dp[0][3], if (W >= enemy[0][0] + enemy[1][0]) 1 else 0)
+
+            for (i in 1 until N) {
+                dp[i][0] = dp[i - 1].maxOrNull()!!
+                dp[i][1] = if (W >= enemy[1][i - 1] + enemy[1][i]) maxOf(dp[i - 1][0], dp[i - 1][2]) + 1 else 0
+                dp[i][2] = if (W >= enemy[0][i - 1] + enemy[0][i]) maxOf(dp[i - 1][0], dp[i - 1][1]) + 1 else 0
+                dp[i][3] = if (W >= enemy[0][i] + enemy[1][i]) dp[i][0] + 1 else 0
+                dp[i][3] = if (W >= enemy[1][i - 1] + enemy[1][i] && W >= enemy[0][i - 1] + enemy[0][i]) maxOf(dp[i][3], dp[i - 1][0] + 2) else dp[i][3]
+            }
+
+            maxPair = maxOf(maxPair, dp[N - 1].maxOrNull()!!)
+        }
     }
 
-    dp2[0][0] = 0
-    dp2[0][3] = if(W >= enemy[0][0] + enemy[1][0]) 1 else 0
-
-    for(i in 1 until N){
-        dp2[i][0] = dp2[i - 1].maxOrNull()!!
-        dp2[i][1] = if(W >= enemy[1][i - 1] + enemy[1][i]) maxOf(dp2[i - 1][0], dp2[i - 1][2]) + 1 else 0
-        dp2[i][2] = if(W >= enemy[0][i - 1] + enemy[0][i]) maxOf(dp2[i - 1][0], dp2[i - 1][1]) + 1 else 0
-        dp2[i][3] = if(W >= enemy[0][i] + enemy[1][i]) dp2[i][0] + 1 else 0
-        dp2[i][3] = if(W >= enemy[1][i - 1] + enemy[1][i] && W >= enemy[0][i - 1] + enemy[0][i]) maxOf(dp2[i][3], dp2[i - 1][0] + 2) else dp2[i][3]
-    }
-
-    return 2 * N - maxOf(dp[N - 2].maxOrNull()!!, dp2[N - 1].maxOrNull()!!)
+    return 2 * N - maxPair
 }
 
 fun main() {
@@ -40,10 +63,13 @@ fun main() {
     repeat(T){
         val (N, W) = br.readLine().split(" ").map{it.toInt()}
 
-        val enemy = Array<List<Int>>(2){List<Int>(N + 1){0} }
+        val enemy = Array<ArrayList<Int>>(2){ArrayList<Int>()}
 
-        for(i in 0 until 2)
-            enemy[i] = br.readLine().split(" ").map { it.toInt()}
+        for(i in 0 until 2) {
+            br.readLine().split(" ").map { it.toInt() }.forEach(){
+                enemy[i].add(it)
+            }
+        }
 
         println(sol(enemy, W))
     }
