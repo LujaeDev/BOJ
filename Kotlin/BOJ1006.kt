90490d1f525f5f97cc87fa17240a7183e4af6622
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2021-01-16 22:53:26 +0900

    Update BOJ1006.kt
    더 수정해야됨

--- Kotlin/BOJ1006.kt
+++ Kotlin/BOJ1006.kt
@@ -2,86 +2,35 @@ import java.io.*
 
 fun sol(enemy: Array<List<Int>>, W: Int): Int{
     val N = enemy[0].size
-    var ret = 0
-    var usedLast0 = false
-    var usedLast1 = false
 
-    val dp = Array<Int>(N + 1){0}
-    val flag = Array<Array<Boolean>>(2){Array<Boolean>(N){false}}
+    val dp = Array<Array<Int>>(N + 1){ Array<Int>(4){0} }
+    val dp2 = Array<Array<Int>>(N + 1){ Array<Int>(4){0} }
 
-    dp[0] = if(W >= enemy[0][0] + enemy[1][0]) 1 else 0
-    flag[0][0] = dp[0] == 1
-    flag[1][0] = dp[0] == 1
+    dp[0][0] = if(W >= enemy[0][N - 1] + enemy[1][N - 1]) 1 else 0
+    dp[0][1] = if(W >= enemy[1][0] + enemy[1][N - 1]) 1 else 0
+    dp[0][2] = if(W >= enemy[0][0] + enemy[0][N - 1]) 1 else 0
+    dp[0][3] = maxOf(if(W >= enemy[0][0] + enemy[1][0]) 1 else 0, if(dp[0][1] + dp[0][2] == 2) 2 else 0)
 
-    if(N > 1) {
-        dp[1] = dp[0] + if (W >= enemy[0][1] + enemy[1][1]) 1 else 0
-        dp[1] = maxOf(dp[1], if (W >= enemy[0][0] + enemy[0][1]) 1 else 0 + if (W >= enemy[1][0] + enemy[1][1]) 1 else 0)
-
-        if ((dp[0] + if (W >= enemy[0][1] + enemy[1][1]) 1 else 0) == if (W >= enemy[0][0] + enemy[0][1]) 1 else 0 + if (W >= enemy[1][0] + enemy[1][1]) 1 else 0) {
-            val cnt1 = if (flag[0][0]) 1 else 0 + if (flag[1][0]) 1 else 0
-            val cnt2 = if (W >= enemy[0][0] + enemy[0][1]) 1 else 0 + if (W >= enemy[1][0] + enemy[1][1]) 1 else 0
-
-            if (cnt2 <= cnt1) {
-                flag[0][1] = W >= (enemy[0][0] + enemy[0][1])
-                flag[1][1] = W >= (enemy[1][0] + enemy[1][1])
-            } else {
-                flag[0][1] = flag[0][0]
-                flag[1][1] = flag[1][0]
-            }
-        } else if (dp[1] == if (W >= enemy[0][0] + enemy[0][1]) 1 else 0 + if (W >= enemy[1][0] + enemy[1][1]) 1 else 0) {
-            flag[0][1] = W >= enemy[0][0] + enemy[0][1]
-            flag[1][1] = W >= enemy[1][0] + enemy[1][1]
-        } else {
-            flag[0][1] = flag[0][0]
-            flag[1][1] = flag[1][0]
-        }
-
-        for (i in 2 until N) {
-            dp[i] = dp[i - 1] + if (W >= enemy[0][i] + enemy[1][i]) 1 else 0
-            dp[i] = maxOf(dp[i], dp[i - 2] + if (W >= enemy[0][i - 1] + enemy[0][i]) 1 else 0 + if (W >= enemy[1][i - 1] + enemy[1][i]) 1 else 0)
-
-            if ((dp[i - 1] + if (W >= enemy[0][i] + enemy[1][i]) 1 else 0) == dp[i - 2] + if (W >= enemy[0][i - 1] + enemy[0][i]) 1 else 0 + if (W >= enemy[1][i - 1] + enemy[1][i]) 1 else 0) {
-                val cnt1 = if (flag[0][i - 1]) 1 else 0 + if (flag[1][i - 1]) 1 else 0
-                val cnt2 = if (flag[0][i - 2]) 1 else 0 + if (flag[1][i - 2]) 1 else 0
-
-                if (cnt2 <= cnt1) {
-                    flag[0][i] = flag[0][i - 2]
-                    flag[1][i] = flag[1][i - 2]
-
-                    usedLast0 = W >= enemy[0][i - 1] + enemy[0][i]
-                    usedLast1 = W >= enemy[1][i - 1] + enemy[1][i]
-                } else {
-                    flag[0][i] = flag[0][i - 1]
-                    flag[1][i] = flag[1][i - 1]
-
-                    usedLast0 = W >= enemy[0][i] + enemy[1][i]
-                    usedLast1 = W >= enemy[0][i] + enemy[1][i]
-                }
-            } else if (dp[i] == dp[i - 2] + if (W >= enemy[0][i - 1] + enemy[0][i]) 1 else 0 + if (W >= enemy[1][i - 1] + enemy[1][i]) 1 else 0) {
-                flag[0][i] = flag[0][i - 2]
-                flag[1][i] = flag[1][i - 2]
-
-
-                usedLast0 = W >= enemy[0][i - 1] + enemy[0][i]
-                usedLast1 = W >= enemy[1][i - 1] + enemy[1][i]
-            } else {
-                flag[0][i] = flag[0][i - 1]
-                flag[1][i] = flag[1][i - 1]
-
-                usedLast0 = W >= enemy[0][i] + enemy[1][i]
-                usedLast1 = W >= enemy[0][i] + enemy[1][i]
-            }
-        }
+    for(i in 1 until N - 1){
+        dp[i][0] = dp[i - 1].maxOrNull()!!
+        dp[i][1] = if(W >= enemy[1][i - 1] + enemy[1][i]) maxOf(dp[i - 1][0], dp[i - 1][2]) + 1 else 0
+        dp[i][2] = if(W >= enemy[0][i - 1] + enemy[0][i]) maxOf(dp[i - 1][0], dp[i - 1][1]) + 1 else 0
+        dp[i][3] = if(W >= enemy[0][i] + enemy[1][i]) dp[i][0] + 1 else 0
+        dp[i][3] = if(W >= enemy[1][i - 1] + enemy[1][i] && W >= enemy[0][i - 1] + enemy[0][i]) maxOf(dp[i][3], dp[i - 1][0] + 2) else dp[i][3]
     }
-    dp[N] = dp[N - 1]
 
-    if(N > 1) {
-        if (!flag[0][N - 1] && !usedLast0)
-            dp[N] += if (W >= enemy[0][N - 1] + enemy[0][0]) 1 else 0
-        if (!flag[1][N - 1] && !usedLast1)
-            dp[N] += if (W >= enemy[1][N - 1] + enemy[1][0]) 1 else 0
+    dp2[0][0] = 0
+    dp2[0][3] = if(W >= enemy[0][0] + enemy[1][0]) 1 else 0
+
+    for(i in 1 until N){
+        dp2[i][0] = dp2[i - 1].maxOrNull()!!
+        dp2[i][1] = if(W >= enemy[1][i - 1] + enemy[1][i]) maxOf(dp2[i - 1][0], dp2[i - 1][2]) + 1 else 0
+        dp2[i][2] = if(W >= enemy[0][i - 1] + enemy[0][i]) maxOf(dp2[i - 1][0], dp2[i - 1][1]) + 1 else 0
+        dp2[i][3] = if(W >= enemy[0][i] + enemy[1][i]) dp2[i][0] + 1 else 0
+        dp2[i][3] = if(W >= enemy[1][i - 1] + enemy[1][i] && W >= enemy[0][i - 1] + enemy[0][i]) maxOf(dp2[i][3], dp2[i - 1][0] + 2) else dp2[i][3]
     }
-    return 2 * N - dp[N]
+
+    return 2 * N - maxOf(dp[N - 2].maxOrNull()!!, dp2[N - 1].maxOrNull()!!)
 }
 
 fun main() {
