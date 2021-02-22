cffb4be0d810387f5d0be2d06d52c7b81286af1d
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2021-02-23 00:41:21 +0900

    BOJ PS

---
+++ Python/BOJ16287.py
@@ -0,0 +1,18 @@
+W, N = map(int, input().split(" "))
+pack = list(map(int, input().split(" ")))
+
+cache = [0 for i in range(0, 80001)]
+
+for i in range(0, N):
+    for j in range(i + 1, N):
+        cache[pack[i] + pack[j]] = i
+
+ret = False
+for i in range(0, N):
+    for j in range(i + 1, N):
+        s = pack[i] + pack[j]
+
+        if W - s > 0 and cache[W - s] > j:
+            ret = True
+
+print("YES" if ret else "NO")
