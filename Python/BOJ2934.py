d54ed7b6a6e4daee3e2898da67b20fac1b7f05fc
Lee-you-jae <62204810+Lee-you-jae@users.noreply.github.com>
2021-05-28 18:39:58 +0900

    BOJ PS

--- Python/BOJ2934.py
+++ Python/BOJ2934.py
@@ -5,7 +5,7 @@ class Seg:
     def __init__(self, N):
         self.tree = [0 for i in range(4 * (N + 1))]
 
-    def update_lazy(self, node, s, e, l, r):
+    def update_lazy(self, node, s, e):
         if self.tree[node] != 0:
             if s != e:
                 self.tree[2 * node] += self.tree[node]
@@ -13,7 +13,7 @@ class Seg:
                 self.tree[node] = 0
 
     def update_range(self, node, s, e, l, r):
-        self.update_lazy(node, s, e, l, r)
+        self.update_lazy(node, s, e)
 
         if e < l or s > r:
             return
@@ -21,7 +21,8 @@ class Seg:
             if s != e:
                 self.tree[2 * node] += 1
                 self.tree[2 * node + 1] += 1
-
+            else:
+                self.tree[node] += 1
             return
         
         mid = int((s + e) / 2)
@@ -29,7 +30,7 @@ class Seg:
         self.update_range(2 * node + 1, mid + 1, e, l , r)
         
     def query(self, node, s, e, pos):
-        self.update_lazy(node, s, e, pos, pos)
+        self.update_lazy(node, s, e)
         
         if s > pos or e < pos:
             return 0
@@ -41,9 +42,11 @@ class Seg:
         
         mid = int((s + e) / 2)
         return self.query(2 * node, s, mid, pos) + self.query(2 * node + 1, mid + 1, e, pos)
-
+            
 N = 100000
 M = int(input())
+tmp = [0 for i in range(100)]
+
 seg = Seg(N)
 
 for i in range(M):
@@ -51,8 +54,9 @@ for i in range(M):
     
     if R > L + 1:
         seg.update_range(1, 1, N, L + 1, R - 1)
-        
+    
     cntL1 = seg.query(1, 1, N, L)
     cntR1 = seg.query(1, 1, N, R)
+    
     
     print(cntL1 + cntR1)
